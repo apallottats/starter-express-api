39fdc9789976d3f8402226988b9224819e89d16a
apallottats <a.pallotta.ext@teamsystem.com>
2022-12-07 12:07:03 +0100

    testing

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
-})
-app.listen(process.env.PORT || 3000)
+const mongoose = require("mongoose");
+const express = require("express");
+const app = express();
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+app.all("/", (req, res) => {
+  console.log("Just got a request!");
+  res.send("Yo!");
+});
+
+connectDB().then(() => {
+  app.listen(process.env.PORT || 3000);
+});
